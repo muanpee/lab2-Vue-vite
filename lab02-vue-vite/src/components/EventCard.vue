696188cc4f3c2b50c7fe0dc0defd0bfe1b49791d
muanpee <muanpee_t@cmu.ac.th>
2025-07-03 22:20:12 +0700

    add event card 7

--- lab02-vue-vite/src/components/EventCard.vue
+++ lab02-vue-vite/src/components/EventCard.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const event = ref({
+  id: 5928101,
+  category:'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee',
+})
 // defineProps<{
 //   msg: string
 // }>()
 </script>
 
 <template>
-  <div class="greetings">
+  <div class="event-class">
+    <div class="event-card">
+     <h2> {{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
   </div>
 </template>
 
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px rgba(0, 0, 0, 0.2);
+}
+</style>
--- lab02-vue-vite/src/views/HomeView.vue
+++ lab02-vue-vite/src/views/HomeView.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+
+</script>
 
 <template>
-<div class="home"></div>
+<div class="home">
+  <EventCard />
+</div>
 </template>
