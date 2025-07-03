c20de792d745f50a3e2c2fa0163bcdcf92556fc0
muanpee <muanpee_t@cmu.ac.th>
2025-07-03 22:47:03 +0700

    add event object 8.4

--- lab02-vue-vite/src/components/EventCard.vue
+++ lab02-vue-vite/src/components/EventCard.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
 import { ref } from 'vue'
-const event = ref({
-  id: 5928101,
-  category:'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee',
-})
-// defineProps<{
-//   msg: string
-// }>()
+import type { Event } from '@/types'
+defineProps<{
+  event: Event
+}>()
+// const event = ref({
+  // id: 5928101,
+  // category:'animal welfare',
+  // title: 'Cat Adoption Day',
+  // description: 'Find your new feline friend at this event.',
+  // location: 'Meow Town',
+  // date: 'January 28, 2022',
+  // time: '12:00',
+  // petsAllowed: true,
+  // organizer: 'Kat Laydee',
+// })
+
 </script>
 
 <template>
