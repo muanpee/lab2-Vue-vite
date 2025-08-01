7a0497ce5cb38e3dee0e5d9c01310c2b2ae9d36e
muanpee <muanpee_t@cmu.ac.th>
2025-08-01 09:38:54 +0700

    fix analytics

--- lab02-vue-vite/src/main.ts
+++ lab02-vue-vite/src/main.ts
@@ -3,12 +3,12 @@
 import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
 const app = createApp(App)
-import { inject } from '@vercel/analytics'
+
 inject()
 app.use(createPinia())
 app.use(router)
